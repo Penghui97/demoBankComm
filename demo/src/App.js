ba049b686a4c57c6af56d5d95bcafe1146cd908f
xia <xw225225@163.com>
2023-10-11 21:01:56 +0800

    get都弄完了

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,4 +1,4 @@
-import {Calendar, Button, Space, Badge} from 'antd';
+import {Calendar, Button, Space, Badge, Modal} from 'antd';
 import {Component} from 'react';
 import moment from 'moment'
 import Controller from './controller'
@@ -74,6 +74,120 @@ class App extends Component {
         })
     }
 
+    infoMaxCount = async (value, type) => {
+        let maxCount = await Controller.requestMax(value, type)
+        console.log("maxCount", maxCount)
+        Modal.info({
+            title: '',
+            content: (
+                <div>
+                    <p>最大连续签到天数为：{maxCount}</p>
+                </div>
+            ),
+            onOk() {
+            },
+        });
+    };
+
+    infoCount = async (value, type) => {
+        let Count = await Controller.requestCount(value, type)
+        Modal.info({
+            title: '',
+            content: (
+                <div>
+                    <p>月签到天数为：{Count}</p>
+                </div>
+            ),
+            onOk() {
+            },
+        });
+    };
+
+    infoGetList = async () => {
+        let data = await Controller.requestGetList()
+        Modal.info({
+            title: '',
+            content: (
+                <div>
+                    <p>老签到系统实现获取签到列表的总耗时为{data.old_sign_total}ms</p>
+                    <p>新签到系统实现获取签到列表的总耗时为{data.new_sign_total}ms</p>
+                    <p>新老系统实现的耗时差为{data.difference_total}ms</p>
+                    <p>耗时优化率为{data.optimized_rate}</p>
+                </div>
+            ),
+            onOk() {
+            },
+        });
+    };
+
+    infoGetCount = async () => {
+        let data = await Controller.requestGetCount()
+        Modal.info({
+            title: '',
+            content: (
+                <div>
+                    <p>老签到系统获取月签到次数的总耗时为{data.old_sign_total}ms</p>
+                    <p>新签到系统获取月签到次数的总耗时为{data.new_sign_total}ms</p>
+                    <p>新老系统实现的耗时差为{data.difference_total}ms</p>
+                    <p>耗时优化率为{data.optimized_rate}</p>
+                </div>
+            ),
+            onOk() {
+            },
+        });
+    };
+
+    infoGetMax = async () => {
+        let data = await Controller.requestGetMax()
+        Modal.info({
+            title: '',
+            content: (
+                <div>
+                    <p>老签到系统获取当月最大连续签到的总耗时为{data.old_sign_total}ms</p>
+                    <p>新签到系统获取当月最大连续签到的总耗时为{data.new_sign_total}ms</p>
+                    <p>新老系统实现的耗时差为{data.difference_total}ms</p>
+                    <p>耗时优化率为{data.optimized_rate}</p>
+                </div>
+            ),
+            onOk() {
+            },
+        });
+    };
+
+    infoGetSign = async () => {
+        let data = await Controller.requestGetSign()
+        Modal.info({
+            title: '',
+            content: (
+                <div>
+                    <p>老签到系统实现签到的总耗时为{data.old_sign_total}ms</p>
+                    <p>新签到系统实现签到的总耗时为{data.new_sign_total}ms</p>
+                    <p>新老系统实现的耗时差为{data.difference_total}ms</p>
+                    <p>耗时优化率为{data.optimized_rate}</p>
+                </div>
+            ),
+            onOk() {
+            },
+        });
+    };
+
+    infoGetSupplementary = async () => {
+        let data = await Controller.requestGetSupplementary()
+        Modal.info({
+            title: '',
+            content: (
+                <div>
+                    <p>老签到系统实现补签的总耗时为{data.old_sign_total}ms</p>
+                    <p>新签到系统实现补签的总耗时为{data.new_sign_total}ms</p>
+                    <p>新老系统实现的耗时差为{data.difference_total}ms</p>
+                    <p>耗时优化率为{data.optimized_rate}</p>
+                </div>
+            ),
+            onOk() {
+            },
+        });
+    };
+
     async componentDidMount() {
         let month = moment().month()
         this.setState({
@@ -97,20 +211,33 @@ class App extends Component {
 
             <div className="App">
                 <div className="calendar">
-                    <Space wrap>
-                        <Button type="primary"
-                                onClick={() => Controller.oldSignIn(new Date().getDate(), this.state.currentMonth)}>老系统签到</Button>
-                        <Button type="primary" onClick={()=>Controller.oldSupplementary(this.state.date,this.state.currentMonth)}>老系统补签</Button>
-                        <Button type="primary">最大连续签到天数</Button>
-                        <Button type="primary">月签到天数</Button>
-                    </Space>
-                    <Space wrap>
-                        <Button type="primary"
-                                onClick={() => Controller.oldSignIn(new Date().getDate(), this.state.currentMonth)}>新系统签到</Button>
-                        <Button type="primary" onClick={()=>Controller.oldSupplementary(this.state.date,this.state.currentMonth)}>新系统补签</Button>
-                        <Button type="primary">最大连续签到天数</Button>
-                        <Button type="primary">月签到天数</Button>
-                    </Space>
+                    <div style={{margin:'15px'}}>
+                        <Space wrap>
+                            <Button onClick={() => this.infoGetList()}>获取签到耗时</Button>
+                            <Button onClick={() => this.infoGetCount()}>月签到次数耗时</Button>
+                            <Button onClick={() => this.infoGetMax()}>最大连续签到耗时</Button>
+                            <Button onClick={() => this.infoGetSign()}>签到耗时</Button>
+                            <Button onClick={() => this.infoGetSupplementary()}>补签耗时</Button>
+                        </Space>
+                    </div>
+                    <div style={{margin:'15px'}}>
+                        <Space wrap>
+                            <Button type="primary"
+                                    onClick={() => Controller.oldSignIn(new Date().getDate(), this.state.currentMonth)}>老系统签到</Button>
+                            <Button type="primary" onClick={()=>Controller.oldSupplementary(this.state.date,this.state.currentMonth)}>老系统补签</Button>
+                            <Button onClick={() => this.infoMaxCount(this.state.currentMonth,1)}>最大连续签到天数</Button>
+                            <Button onClick={() => this.infoCount(this.state.currentMonth,1)}>月签到天数</Button>
+                        </Space>
+                    </div>
+                    <div style={{margin:'15px'}}>
+                        <Space wrap>
+                            <Button type="primary"
+                                    onClick={() => Controller.oldSignIn(new Date().getDate(), this.state.currentMonth)}>新系统签到</Button>
+                            <Button type="primary" onClick={()=>Controller.oldSupplementary(this.state.date,this.state.currentMonth)}>新系统补签</Button>
+                            <Button onClick={() => this.infoMaxCount(this.state.currentMonth,2)}>最大连续签到天数</Button>
+                            <Button onClick={() => this.infoCount(this.state.currentMonth,2)}>月签到天数</Button>
+                        </Space>
+                    </div>
                     <Calendar
                         onPanelChange={this.onPanelChange}
                         cellRender={this.cellRender}
